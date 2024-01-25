1d5985efd73d7d7f7b8333d9c5f0beaf13c32329
motyUpstay <99587800+motyUpstay@users.noreply.github.com>
2024-01-25 11:44:37 +0200

    update url

--- api/menu.js
+++ api/menu.js
@@ -1,46 +1,47 @@
-
 function generateRandomString(length) {
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-let randomString = '';
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let randomString = "";
 
-for (let i = 0; i < length; i++) {
+  for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
     randomString += characters.charAt(randomIndex);
+  }
+
+  return randomString;
 }
 
-return randomString;
+function genrateRandomNumber(depth) {
+  if (depth === 0) return 4;
+  return Math.floor(Math.random() * 6) + 2;
 }
-
-function genrateRandomNumber (depth) {
-    if (depth === 0) return 4;
-    return Math.floor(Math.random() * 6) + 2;
-};
 
 export const menuInfo = (stop_fake_errors) => {
+  if (!stop_fake_errors) {
+    const throwFakeError = Math.random() < 0.5 ? 0 : 1;
+    if (throwFakeError) throw new Error("Something went wrong :(");
+  }
 
-    if(!stop_fake_errors) {
-        const throwFakeError = Math.random() < 0.5 ? 0 : 1;
-        if (throwFakeError) throw new Error('Something went wrong :(')
-    }
+  const rowWithRandomNames = (depth) => {
+    const hasChildren = Math.random() < 0.5 ? 0 : 1;
+    if ((depth > 0 && !hasChildren) || depth > 8) return;
+    const rowArray = new Array(genrateRandomNumber(depth)).fill(1);
+    return rowArray.map(() => {
+      const randomWord = generateRandomString(5);
+      return {
+        displayName: randomWord,
+        url: `/${randomWord}`.toLowerCase(),
+        children: rowWithRandomNames(depth + 1),
+      };
+    });
+  };
 
-    const rowWithRandomNames = (depth) => {
-        const hasChildren = Math.random() < 0.5 ? 0 : 1;
-        if( (depth > 0 && !hasChildren) || depth > 8) return;
-        const rowArray = new Array(genrateRandomNumber(depth)).fill(1);
-        return rowArray.map(() => {
-            const randomWord = generateRandomString(5);
-            return {
-                displayName: randomWord,
-                url: `/${randomWord}`,
-                children: rowWithRandomNames(depth + 1)
-            };
-        });
-    }
+  const baseMenuStructure = rowWithRandomNames(0);
 
-    const baseMenuStructure = rowWithRandomNames(0)
+  const menuStructure = [
+    { displayName: "home", url: "/" },
+    { displayName: "team", url: "/team" },
+    ...baseMenuStructure,
+  ];
 
-    const menuStructure = [{displayName: "home", url: "/"}, {displayName: "team", url: "/team"}, ...baseMenuStructure]
-
-    return menuStructure
-
-}
+  return menuStructure;
+};
